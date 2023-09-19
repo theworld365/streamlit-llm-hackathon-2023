c0344725d7f62a32c31562c7c7db7d2abef9f316
saargunam-personal <145439347+saargunam-personal@users.noreply.github.com>
2023-09-19 16:34:24 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,29 +16,39 @@ def load_csv(input_csv):
   return df
 
 # Generate LLM response
-def generate_response(csv_file, input_query):
+def generate_response(csv_file, input_query, responses):
   llm = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.2, openai_api_key=openai_api_key)
   df = load_csv(csv_file)
   # Create Pandas DataFrame Agent
   agent = create_pandas_dataframe_agent(llm, df, verbose=True, agent_type=AgentType.OPENAI_FUNCTIONS)
   # Perform Query using the Agent
   response = agent.run(input_query)
-  return st.success(response)
+  responses.append(response)  # Append the response to the list of responses
+  return responses
 
 # Input widgets
 uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
 question_list = [
-  'Provide recommendations to optimise the resources and save the cloud cost bill',
-  'Generate terraform code template based on the recommendations?',
+  'Provide recommendations to optimize the resources and save the cloud cost bill',
+  'Generate Terraform code template based on the recommendations?',
   'Other']
 query_text = st.selectbox('Select an example query:', question_list, disabled=not uploaded_file)
 openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
 
+# Initialize responses list
+responses = []
+
 # App logic
-if query_text is 'Other':
-  query_text = st.text_input('Enter your query:', placeholder = 'Enter query here ...', disabled=not uploaded_file)
+if query_text == 'Other':
+  query_text = st.text_input('Enter your query:', placeholder='Enter query here ...', disabled=not uploaded_file)
 if not openai_api_key.startswith('sk-'):
   st.warning('Please enter your OpenAI API key!', icon='⚠')
 if openai_api_key.startswith('sk-') and (uploaded_file is not None):
-  st.header('Output')
-  generate_response(uploaded_file, query_text)
+  if st.button('Generate Response'):
+    responses = generate_response(uploaded_file, query_text, responses)
+
+# Display previous responses
+if responses:
+  st.header('Previous Responses')
+  for i, response in enumerate(responses):
+    st.success(f'Response {i+1}: {response}')
