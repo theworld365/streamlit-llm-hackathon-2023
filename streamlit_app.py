f4f38185b1d6ddacf26602cb22ef58e9f358272f
saargunam-personal <145439347+saargunam-personal@users.noreply.github.com>
2023-09-19 14:48:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,44 +1,45 @@
 import streamlit as st
-
-from langchain.agents import initialize_agent, AgentType
-from langchain.callbacks import StreamlitCallbackHandler
+import pandas as pd
 from langchain.chat_models import ChatOpenAI
-from langchain.tools import DuckDuckGoSearchRun
+from langchain.agents import create_pandas_dataframe_agent
+from langchain.agents.agent_types import AgentType
 
-with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key", key="langchain_search_api_key_openai", type="password")
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/2_Chat_with_search.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+# Page title
+st.set_page_config(page_title='🦜🔗 Ask the Data App')
+st.title('🦜🔗 Ask the Data App')
 
-st.title("🔎 LangChain - Chat with search")
+# Load CSV file
+def load_csv(input_csv):
+  df = pd.read_csv(input_csv)
+  with st.expander('See DataFrame'):
+    st.write(df)
+  return df
 
-"""
-In this example, we're using `StreamlitCallbackHandler` to display the thoughts and actions of an agent in an interactive Streamlit app.
-Try more LangChain 🤝 Streamlit Agent examples at [github.com/langchain-ai/streamlit-agent](https://github.com/langchain-ai/streamlit-agent).
-"""
+# Generate LLM response
+def generate_response(csv_file, input_query):
+  llm = ChatOpenAI(model_name='gpt-3.5-turbo-0613', temperature=0.2, openai_api_key=openai_api_key)
+  df = load_csv(csv_file)
+  # Create Pandas DataFrame Agent
+  agent = create_pandas_dataframe_agent(llm, df, verbose=True, agent_type=AgentType.OPENAI_FUNCTIONS)
+  # Perform Query using the Agent
+  response = agent.run(input_query)
+  return st.success(response)
 
-if "messages" not in st.session_state:
-    st.session_state["messages"] = [
-        {"role": "assistant", "content": "Hi, I'm a chatbot who can search the web. How can I help you?"}
-    ]
+# Input widgets
+uploaded_file = st.file_uploader('Upload a CSV file', type=['csv'])
+question_list = [
+  'How many rows are there?',
+  'What is the range of values for MolWt with logS greater than 0?',
+  'How many rows have MolLogP value greater than 0.',
+  'Other']
+query_text = st.selectbox('Select an example query:', question_list, disabled=not uploaded_file)
+openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
 
-for msg in st.session_state.messages:
-    st.chat_message(msg["role"]).write(msg["content"])
-
-if prompt := st.chat_input(placeholder="Who won the Women's U.S. Open in 2018?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
-
-    llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai_api_key, streaming=True)
-    search = DuckDuckGoSearchRun(name="Search")
-    search_agent = initialize_agent([search], llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, handle_parsing_errors=True)
-    with st.chat_message("assistant"):
-        st_cb = StreamlitCallbackHandler(st.container(), expand_new_thoughts=False)
-        response = search_agent.run(st.session_state.messages, callbacks=[st_cb])
-        st.session_state.messages.append({"role": "assistant", "content": response})
-        st.write(response)
+# App logic
+if query_text is 'Other':
+  query_text = st.text_input('Enter your query:', placeholder = 'Enter query here ...', disabled=not uploaded_file)
+if not openai_api_key.startswith('sk-'):
+  st.warning('Please enter your OpenAI API key!', icon='⚠')
+if openai_api_key.startswith('sk-') and (uploaded_file is not None):
+  st.header('Output')
+  generate_response(uploaded_file, query_text)
